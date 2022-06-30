7b7f70811605938c080f6c95b6f7623709149b28
jshiohaha <dev@jacobshiohira.com>
2022-06-30 00:01:17 -0500

    change

--- bob.sh
+++ bob.sh
@@ -1,5 +1,18 @@
-pkgs="candy-machine token-metadata"
+pkgs="candy-machine"
 pkgs=(${pkgs//\"})
 for pkg in "${pkgs[@]}"; do
-  echo "$pkg"
+  echo ">> changing dir $pkg/js"
+  cd "$pkg/js"
+  echo ">> yarn install && solita"
+  # yarn install && yarn add -D @metaplex-foundation/solita && yarn api:gen
+  # git restore package.json
+  echo "=============================="
+  git status
+  echo "=============================="
+  if [[ $(git diff --stat) != '' ]]; then
+    # echo "::set-output name=failed::true"
+    echo "dirty"
+    break
+  fi
+  cd ../..
 done
