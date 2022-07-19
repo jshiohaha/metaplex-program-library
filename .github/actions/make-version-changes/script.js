0f501832c5fff712a2c1f2799880de9992770d17
jshiohaha <dev@jacobshiohira.com>
2022-07-18 23:02:38 -0500

    version workflow

--- .github/actions/make-version-changes/script.js
+++ .github/actions/make-version-changes/script.js
@@ -95,51 +95,64 @@ const updateCratesPackage = async (io, cwdArgs, pkg, semvar) => {
   console.log('updating rust package');
   const currentDir = cwdArgs.join('/');
 
-  // adds git info automatically
+  // // adds git info automatically
+  // wrappedExec(
+  //   `cargo release --no-publish --no-push --no-confirm --verbose --execute --no-verify --no-tag --config ../../release.toml ${semvar}`,
+  //   currentDir,
+  // );
+
+  // const sourceIdlDir = [...cwdArgs.slice(0, cwdArgs.length - 2), 'target', 'idl'].join('/');
+
+  // // generate IDL
+  // if (packageHasIdl(pkg)) {
+  //   // replace all instances of - with _
+  //   let idlName = `${pkg.replace(/\-/g, '_')}.json`;
+  //   if (packageUsesAnchor(pkg)) {
+  //     console.log(
+  //       'generate IDL via anchor',
+  //       wrappedExec(`~/.cargo/bin/anchor build --skip-lint --idl ${sourceIdlDir}`, currentDir),
+  //     );
+  //   } else {
+  //     console.log(
+  //       'generate IDL via shank',
+  //       wrappedExec(`~/.cargo/bin/shank idl --out-dir ${sourceIdlDir}  --crate-root .`, currentDir),
+  //     );
+  //     idlName = `mpl_${idlName}`;
+  //   }
+
+  //   // create ../js/idl dir if it does not exist; back one dir + js dir + idl dir
+  //   // note: cwdArgs == currentDir.split("/")
+  //   const destIdlDir = [...cwdArgs.slice(0, cwdArgs.length - 1), 'js', 'idl'].join('/');
+
+  //   if (!fs.existsSync(destIdlDir)) {
+  //     console.log(`creating ${destIdlDir} because it DNE`, await io.mkdirP(destIdlDir));
+  //   }
+
+  //   console.log('final IDL name: ', idlName);
+  //   // cp IDL to js dir
+  //   wrappedExec(`cp ${sourceIdlDir}/${idlName} ${destIdlDir}`, currentDir);
+
+  //   // append IDL change to rust version bump commit
+  //   wrappedExec(`git add ${destIdlDir} && git commit --amend -C HEAD && git push`);
+  // } else {
+  //   wrappedExec('git push');
+  // }
+
   wrappedExec(
-    `cargo release --no-publish --no-push --no-confirm --verbose --execute --no-verify --no-tag --config ../../release.toml ${semvar}`,
+    `echo "rust ${semvar} changes" >> ./rusty && git add ./rusty && git commit -m "rust change" && git push`,
     currentDir,
   );
-
-  const sourceIdlDir = [...cwdArgs.slice(0, cwdArgs.length - 2), 'target', 'idl'].join('/');
-
-  // generate IDL
-  if (packageHasIdl(pkg)) {
-    // replace all instances of - with _
-    let idlName = `${pkg.replace(/\-/g, '_')}.json`;
-    if (packageUsesAnchor(pkg)) {
-      console.log(
-        'generate IDL via anchor',
-        wrappedExec(`~/.cargo/bin/anchor build --skip-lint --idl ${sourceIdlDir}`, currentDir),
-      );
-    } else {
-      console.log(
-        'generate IDL via shank',
-        wrappedExec(`~/.cargo/bin/shank idl --out-dir ${sourceIdlDir}  --crate-root .`, currentDir),
-      );
-      idlName = `mpl_${idlName}`;
-    }
-
-    // create ../js/idl dir if it does not exist; back one dir + js dir + idl dir
-    // note: cwdArgs == currentDir.split("/")
-    const destIdlDir = [...cwdArgs.slice(0, cwdArgs.length - 1), 'js', 'idl'].join('/');
-
-    if (!fs.existsSync(destIdlDir)) {
-      console.log(`creating ${destIdlDir} because it DNE`, await io.mkdirP(destIdlDir));
-    }
-
-    console.log('final IDL name: ', idlName);
-    // cp IDL to js dir
-    wrappedExec(`cp ${sourceIdlDir}/${idlName} ${destIdlDir}`, currentDir);
-    // append IDL change to rust version bump commit
-    wrappedExec(`git add ${destIdlDir} && git commit --amend -C HEAD && git push`);
-  }
 };
 
 const updateNpmPackage = (cwdArgs, _pkg, semvar) => {
-  console.log(
-    'updating js package',
-    wrappedExec(`yarn install && npm version ${semvar} && git push`, cwdArgs.join('/')),
+  // console.log(
+  //   'updating js package',
+  //   wrappedExec(`yarn install && npm version ${semvar} && git push`, cwdArgs.join('/')),
+  // );
+
+  wrappedExec(
+    `echo "js ${semvar} changes" >> ./javyscripty && git add ./javyscripty && git commit -m "js change" && git push`,
+    currentDir,
   );
 };
 
@@ -157,8 +170,14 @@ const updateNpmPackage = (cwdArgs, _pkg, semvar) => {
  * @param {packages} arr List of packages to process in the form <pkg-name>/<sub-dir>
  * @param {versioning} arr List of version commands in the form semvar:pkg:type where type = `program|js`
  * @return void
+ *
  */
-module.exports = async ({ github, context, core, glob, io }, packages, versioning) => {
+module.exports = async ({ github, context, core, glob, io, source_repo }, packages, versioning) => {
+  console.log('source_repo: ', source_repo);
+  // console.log('github: ', github);
+  // console.log('github.pulls: ', github.pulls);
+  // console.log('github.issues: ', github.issues);
+
   const base = process.env.GITHUB_ACTION_PATH; // alt: path.join(__dirname);
   const splitBase = base.split('/');
   const parentDirsToHome = 4; // ~/<home>/./.github/actions/<name>
@@ -172,6 +191,15 @@ module.exports = async ({ github, context, core, glob, io }, packages, versionin
   // setup git user config
   wrappedExec('git config user.name github-actions[bot]');
   wrappedExec('git config user.email github-actions[bot]@users.noreply.github.com');
+
+  // we can't push direclty to a fork, so we need to open a PR
+  let newBranch;
+  if (isPrFromFork(source_repo.from_repository, source_repo.to_repository)) {
+    // random 8 alphanumeric postfix
+    newBranch = `${source_repo.from_branch}-${(Math.random() + 1).toString(36).substr(2, 10)}`;
+    // if fork, create new branch and register with upstream
+    wrappedExec(`git checkout -b ${newBranch} && git push -u origin ${newBranch}`);
+  }
 
   // versioning = [semvar:pkg:type]
   for (const version of versioning) {
@@ -221,4 +249,35 @@ module.exports = async ({ github, context, core, glob, io }, packages, versionin
       cwdArgs.pop();
     }
   }
+
+  // if fork, create pull request and create comment on soure pull request
+  if (isPrFromFork(source_repo.from_repository, source_repo.to_repository)) {
+    const [fromOwner, fromRepo] = source_repo.from_repository.split('/');
+
+    // pass in the owner + repo + base (derive head) + issue number (PR)
+    const { data: pullRequest } = await github.pulls.create({
+      owner: fromOwner,
+      repo: fromRepo,
+      head: newBranch, // from
+      base: source_repo.from_branch, // to
+      title: `versioning: ${newBranch} to ${source_repo.from_branch}`,
+      body: `Version bump requested on https://github.com/${source_repo.to_repository}/pull/${source_repo.pull_number}`,
+    });
+
+    console.log('source_repo: ', source_repo);
+    console.log('pullRequest: ', pullRequest);
+
+    const [toOwner, toRepo] = source_repo.to_repository.split('/');
+    // https://github.com/${source_repo}/pull/${issue_number}
+    const { data: commentResult } = github.issues.createComment({
+      owner: toOwner, // MPL_OWNER,
+      repo: toRepo, // MPL_REPO,
+      issue_number: source_repo.pull_number,
+      body: `Created a PR with version changes https://github.com/${source_repo.from_repository}/pull/${pullRequest.number}. Please merge to so changes are reflected in this PR.`,
+    });
+
+    console.log('commentResult: ', commentResult);
+  }
 };
+
+const isPrFromFork = (head, base) => true; // todo: head !== base;
