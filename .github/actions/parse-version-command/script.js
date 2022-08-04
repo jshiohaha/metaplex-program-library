0eac1c35d88b5fcaf7b57233494b67c65f70b8cb
jshiohaha <dev@jacobshiohira.com>
2022-08-04 16:13:17 -0500

    test multiline review body

--- .github/actions/parse-version-command/script.js
+++ .github/actions/parse-version-command/script.js
@@ -5,8 +5,10 @@ const VERSIONING_COMMAND = /^version/g;
 const parse = (body) => {
   const trimmedBody = body
     .toLowerCase()
-    .split('\n')
+    .split('\n\r')
     .filter((t) => t.length > 0);
+
+  console.log('trimmedBody: ', trimmedBody);
 
   const validVersionCmds = trimmedBody.filter((c) => VERSIONING_COMMAND.test(c.trim()));
 
